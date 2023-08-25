c3868eeb01c30837f5e9a0838081440179c01da1
Lakshay <lakshaybansal879@gmail.com>
2023-08-25 09:24:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let hrs=document.getElementById("hours");
+let min=document.getElementById("minutes");
+let sec=document.getElementById("seconds");
+let date=document.getElementById("date");
+let day=document.getElementById("day");
+let month=document.getElementById("month");
+let year=document.getElementById("year");
+let currentDate=new Date(); 
+setInterval(function time(){
+    let currentTime=new Date();
+
+hrs.innerText=(currentTime.getHours()<10?"0":"")+currentTime.getHours()+":";
+min.innerText=(currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes()+":";
+sec.innerText=(currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+
+},1000)
+const weekDay=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+const yearMonths=["January","Feburary","March","April","May","June","July","August","September","October","November","Decemember"];
+
+date.innerText=(currentDate.getDate());
+day.innerText=weekDay[currentDate.getDay()];
+month.innerText=yearMonths[currentDate.getMonth()];
+year.innerText=currentDate.getFullYear();
+time();
